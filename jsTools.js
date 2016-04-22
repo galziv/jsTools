02b1004c70dd19868ae0a66b785626335b56eed5
Gal Ziv <gz0220@gmail.com>
2016-04-23 00:03:20 +0300

    friendlier animate

--- jsTools.js
+++ jsTools.js
@@ -28,7 +28,6 @@ var CanvasX = (function () {
                     var newValue = currentValues[i] + delta;
                     parametersArray[valueIndex[i]] = newValue;
                     currentValues[i] = newValue;
-                    this[func].apply(this, parametersArray);
                 }.bind(this));
 
                 this[func].apply(this, parametersArray);
@@ -47,29 +46,56 @@ var CanvasX = (function () {
         });
     };
 
+    var getTransitionedData = function (startParameters, endParameters) {
+
+
+        var result = {
+            parametersArray: null,
+            valuesIndex: [],
+            startValues: [],
+            endValues: []
+        };
+
+        if (!isNaN(Number(startParameters))) {
+            startParameters = [startParameters];
+            endParameters = [endParameters];
+        }
+
+        result.parametersArray = startParameters;
+
+        startParameters.forEach(function (d, i) {
+            if (d != endParameters[i]) {
+                result.valuesIndex.push(i);
+                result.startValues.push(d);
+                result.endValues.push(endParameters[i]);
+            }
+        });
+
+        return result;
+    };
+
     /**
      * creates a animated transition between start and end values in the specifiec duration
      * @param {string} func - function name to execute
-     * @param {Array} parametersArray - function parameters as an array (perhaps not all values need to be incremented/decremented)
-     * @param {(number|Array)} valueIndex - the index of the value needs to be changed in the parametersArray parameter. type of valueIndex, startValue and endValue and  must match.
-     * @param {(number|Array)} startValue - start value. specify a number if only on parameter should be changed. specify an array if several parameters should be changed. type of valueIndex, startValue and endValue and  must match.
-     * @param {(number|Array)} endValue - end value. specify a number if only on parameter should be changed. specify an array if several parameters should be changed. type of valueIndex, startValue and endValue and  must match.
+     * @param {(number|Array)} startParameters - start value. specify a number if only on parameter should be changed. specify an array if several parameters should be changed. type of valueIndex, startValue and endValue and  must match.
+     * @param {(number|Array)} endParameters - end value. specify a number if only on parameter should be changed. specify an array if several parameters should be changed. type of valueIndex, startValue and endValue and  must match.
      * @param {number} durationMs - the duration for the animation to last
      * @returns {} 
      */
-    var animate = function (func, parametersArray, valueIndex, startValue, endValue, durationMs) {
+    var animate = function (func, startParameters, endParameters, durationMs) {
+        
+        var data;
 
-        if (Array.isArray(startValue) || Array.isArray(endValue) || Array.isArray(valueIndex)) {
+        if (Array.isArray(startParameters) || Array.isArray(endParameters)) {
 
-            if (!Array.isArray(startValue) || !Array.isArray(endValue) || !Array.isArray(valueIndex)) {
-                console.error('if any of startValue or endValue specified as array then both parameters have to be an Array');
+            if (!Array.isArray(startParameters) || !Array.isArray(endParameters)) {
+                console.error('if any of startParameters or endParameters specified as array then both parameters have to be an Array');
                 return;
             }
-
-            animateArray(this, func, parametersArray, valueIndex, startValue, endValue, durationMs);
-        } else {
-            animateArray(this, func, parametersArray, [valueIndex], [startValue], [endValue], durationMs);
         }
+
+        data = getTransitionedData(startParameters, endParameters)
+        animateArray(this, func, data.parametersArray, data.valuesIndex, data.startValues, data.endValues, durationMs);
     };
 
     var xpand = function () {
@@ -140,7 +166,5 @@ var LoadTest = (function () {
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
-context.fillRect(0, 0, 1, 100);
-
-//CanvasX.animate(context, 'fillRect', [0, 0, 1, 100], 2, 1, 1000, 1000);
 CanvasX.xpand();
+context.animate('fillRect', [0, 0, 1, 1], [0, 0, 100, 100], 500);
