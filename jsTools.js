abc655c66e17da1a32e5a5025771c77de4e53a38
Gal Ziv <gz0220@gmail.com>
2016-05-19 21:27:06 +0300

    working async series

--- jsTools.js
+++ jsTools.js
@@ -3,19 +3,66 @@ var CanvasX = (function () {
 
     'use strict';
 
-    var getMaxDuration = function (funcArray) {
+    var strokeCircle = function (x, y, radius) {
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI);
+        context.stroke();
+        context.closePath();
+    };
 
-        var max = 0;
+    var fillCircle = function (x, y, radius) {
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI);
+        context.fill();
+        context.closePath();
+    }
+
+    var applyXtension = function (target) {
+        target.animate = animate;
+        target.animateSeries = animateSeries;
+        target.fillCircle = fillCircle;
+        target.strokeCircle = strokeCircle;
+        target.animateAsymmetricSeries = animateAsymmetricSeries;
+    }
+
+    var xpand = function () {
+        applyXtension(CanvasRenderingContext2D.prototype);
+    };
+
+    var xtend = function (context) {
+        applyXtension(context);
+    };
+
+    var CanvasX = function (context) {
+        CanvasX.xtend(context);
+    };
+
+    /**
+     * internal function to get max duration for given functions array and main duration
+     * @param {array} funcArray 
+     * @param {number} duration 
+     * @returns {} 
+     */
+    var getMaxDuration = function (funcArray, duration) {
+
+        var max = duration || 0;
 
         funcArray.forEach(function (d) {
-            if (d.duration > max) {
-                max = d.duration;
+            if (d.endsAt && d.endsAt > max) {
+                max = d.endsAt;
             }
         })
 
         return max;
     };
 
+    /**
+     * Internal function to animate functions object dictionary
+     * @param {object} _this 
+     * @param {obejct} functions 
+     * @param {number} durationMs 
+     * @returns {} 
+     */
     var animateObjArray = function (_this, functions, durationMs) {
 
         return new Promise(function (resolve, reject) {
@@ -25,25 +72,25 @@ var CanvasX = (function () {
             var segments = {};
             var originalStyle = { fill: _this.fillStyle, stroke: _this.strokeStyle };
             var totalDuration = durationMs || getMaxDuration(functions);
+            var completedSegments = 0;
 
             functions.forEach(function (func, i) {
 
                 var objName = func.functionName + i; // in case several segments for the same function are sent, this prevents overwriting.
-                var segment = segments[objName] = segments[objName] || { functionName: func.functionName, diffs: [], valPerMs: [], currentValues: func.data.startValues, data: func.data };
+                var segment;
 
+                segments[objName] = segments[objName] || { functionName: func.functionName, diffs: [], valPerMs: [], currentValues: func.data.startValues, data: func.data };
+                segment = segments[objName];
+
+                // get total value to animate
                 segment.diffs = func.data.startValues.map(function (d, i) {
                     return func.data.endValues[i] - func.data.startValues[i];
                 })
 
-                if (durationMs !== undefined) {
-                    segment.valPerMs = segment.diffs.map(function (d) {
-                        return d / durationMs;
-                    })
-                } else {
-                    segment.valPerMs = segment.diffs.map(function (d) {
-                        return d / func.duration;
-                    });
-                }
+                // calc delta per ms
+                segment.valPerMs = segment.diffs.map(function (d) {
+                    return d / (func.data.endsAt - func.data.startsAt);
+                });
             });
 
             var execute = function () {
@@ -61,8 +108,7 @@ var CanvasX = (function () {
                     segment = segments[name];
                     style = segment.data.style;
 
-
-                    if (!(segment.data.duration && segment.data.duration < startDiff)) {
+                    if (segment.data.startsAt <= startDiff && segment.data.endsAt >= startDiff) {
 
                         segment.diffs.forEach(function (d, i) {
 
@@ -71,8 +117,10 @@ var CanvasX = (function () {
                             var newValue = segment.currentValues[i] + delta;
                             segment.data.parametersArray[segment.data.valuesIndex[i]] = newValue;
                             segment.currentValues[i] = newValue;
-                        }.bind(this));
 
+                            console.log(segment.currentValues);
+
+                        }.bind(this));
                     }
 
                     if (style) {
@@ -97,11 +145,10 @@ var CanvasX = (function () {
 
                 before = now;
 
-                if (startDiff < totalDuration) {
+                if (totalDuration >= startDiff) {
                     window.requestAnimationFrame(execute);
                 } else {
                     // if for example alert is used, then after closing the alert canvas does one more paint
-
                     setTimeout(resolve, 1);
                 }
             }.bind(_this);
@@ -110,6 +157,17 @@ var CanvasX = (function () {
         });
     };
 
+    /**
+     * Internal function to animate single function with parameters as an array
+     * @param {object} _this 
+     * @param {object} func 
+     * @param {array} parametersArray 
+     * @param {array} array with the index of the values which are to be animated. 
+     * @param {array} startValues 
+     * @param {array} endValues 
+     * @param {number} durationMs 
+     * @returns {} 
+     */
     var animateArray = function (_this, func, parametersArray, valuesIndex, startValues, endValues, durationMs) {
 
         return new Promise(function (resolve, reject) {
@@ -156,19 +214,26 @@ var CanvasX = (function () {
         });
     };
 
-    var getSegmentData = function (startParameters, endParameters, style, duration) {
+    /**
+     * Internal function to generate animation segments
+     * @param {array} startParameters 
+     * @param {array} endParameters 
+     * @param {object} style 
+     * @param {number} startsAt 
+     * @param {number} endsAt 
+     * @returns {} 
+     */
+    var getSegmentData = function (startParameters, endParameters, style, startsAt, endsAt) {
 
         var result = {
             parametersArray: null,
             valuesIndex: [],
             startValues: [],
             endValues: [],
-            style: style
+            style: style,
+            startsAt: startsAt,
+            endsAt: endsAt
         };
-
-        if (duration) {
-            result.duration = duration;
-        }
 
         if (!isNaN(Number(startParameters))) {
             startParameters = [startParameters];
@@ -213,56 +278,49 @@ var CanvasX = (function () {
         animateArray(this, func, data.parametersArray, data.valuesIndex, data.startValues, data.endValues, durationMs);
     };
 
+    /**
+     * Public API - animate a synchronized series
+     * @param {array} funcArray 
+     * @param {number} durationMs 
+     * @returns {} 
+     */
     var animateSeries = function (funcArray, durationMs) {
 
         funcArray.forEach(function (d) {
-            d.data = getSegmentData(d.startParameters, d.endParameters, d.style);
+            d.data = getSegmentData(d.startParameters, d.endParameters, d.style, durationMs);
         })
 
         animateObjArray(this, funcArray, durationMs);
     };
 
-    var animateAsymmetricSeries = function (funcArray) {
+    /**
+     * Public API - animate a asynchronized series
+     * @param {array} segmentsArray 
+     * @param {number} [durationMs] - total duration for the series. if a function has an endsAt which is greater than this value will override duraionMs 
+     * @returns {} 
+     */
+    var animateAsymmetricSeries = function (segmentsArray, duration) {
 
-        funcArray.forEach(function (d) {
-            d.data = getSegmentData(d.startParameters, d.endParameters, d.style, d.duration);
+        duration = getMaxDuration(segmentsArray, duration);
+
+        if (duration == 0) {
+            console.warn('animation duration was 0. verify duration was specified or endsAt on segmentObject');
+        }
+
+        segmentsArray.forEach(function (d) {
+
+            if (!d.endsAt) {
+                d.endsAt = duration; // if not specified animate till total animation length
+            }
+
+            if (!d.startsAt) {
+                d.startsAt = 0
+            }
+
+            d.data = getSegmentData(d.startParameters, d.endParameters, d.style, d.startsAt, d.endsAt);
         })
 
-        animateObjArray(this, funcArray);
-    };
-
-    var strokeCircle = function (x, y, radius) {
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI);
-        context.stroke();
-        context.closePath();
-    };
-
-    var fillCircle = function (x, y, radius) {
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI);
-        context.fill();
-        context.closePath();
-    }
-
-    var applyXtension = function (target) {
-        target.animate = animate;
-        target.animateSeries = animateSeries;
-        target.fillCircle = fillCircle;
-        target.strokeCircle = strokeCircle;
-        target.animateAsymmetricSeries = animateAsymmetricSeries;
-    }
-
-    var xpand = function () {
-        applyXtension(CanvasRenderingContext2D.prototype);
-    };
-
-    var xtend = function (context) {
-        applyXtension(context);
-    };
-
-    var CanvasX = function (context) {
-        CanvasX.xtend(context);
+        animateObjArray(this, segmentsArray, duration);
     };
 
     CanvasX.xpand = xpand;
@@ -339,14 +397,28 @@ CanvasX.xpand();
 //context.animateSeries([
 //    { functionName: 'fillCircle', startParameters: [50, 100, 50], endParameters: [1000 - 50, 100, 50], style: { fill: 'red', stroke: 'orange' } },
 //    { functionName: 'fillCircle', startParameters: [50, 200, 50], endParameters: [1000 - 50, 200, 50], style: { fill: 'purple', stroke: 'orange' } },
-//], 11000);
+//], 2000);
 
 
 context.animateAsymmetricSeries([
-    { functionName: 'fillCircle', startParameters: [50, 100, 50], endParameters: [1000 - 50, 100, 50], style: { fill: 'red', stroke: 'orange' }, duration: 2000 },
-    { functionName: 'fillCircle', startParameters: [50, 200, 50], endParameters: [700, 200, 50], style: { fill: 'purple', stroke: 'orange' }, duration: 500 },
-    { functionName: 'fillCircle', startParameters: [50, 0, 50], endParameters: [450, 200, 50], style: { fill: 'cyan', stroke: 'pink' }, duration: 1000 },
-]);
+    { functionName: 'fillCircle', startParameters: [50, 100, 50], endParameters: [1000 - 50, 100, 50], style: { fill: 'red', stroke: 'orange' } },
+    { functionName: 'fillCircle', startParameters: [50, 200, 50], endParameters: [700, 200, 50], style: { fill: 'purple', stroke: 'orange' }, startsAt: 0, endsAt: 1000 },
+    { functionName: 'fillCircle', startParameters: [50, 0, 50], endParameters: [450, 200, 50], style: { fill: 'cyan', stroke: 'pink' }, startsAt: 1000, endsAt: 2000 },
+    { functionName: 'fillRect', startParameters: [0, 0, 1, 1, ], endParameters: [0, 0, 100, 100], style: { fill: 'cyan', stroke: 'pink' }, startsAt: 1000, endsAt: 2000 }
+], 2000);
+
+//context.animateAsymmetricSeries([
+//    {
+//        functionName: 'fillCircle',
+//        startParameters: [50, 100, 50],
+//        endParameters: [1000 - 50, 100, 50],
+//        style: { fill: 'red', stroke: 'orange' },
+//        startsAt: 0,
+//        endsAt: 1000
+//    },
+//    { functionName: 'fillCircle', startParameters: [1000 - 50, 100, 50], endParameters: [50, 100, 50], style: { fill: 'red', stroke: 'orange' }, startsAt: 1000, endsAt: 2000 },
+
+//]);
 
 
 
